ca3388b96c722a49937d655ef3b28df007686dd6
surcyf123 <114649324+surcyf123@users.noreply.github.com>
2025-03-07 15:52:32 -0500

    add batch transfer

--- src/core/utils.py
+++ src/core/utils.py
@@ -39,6 +39,71 @@ def convert_to_alpha(substrate: SubstrateInterface, amount_in_tao: float) -> flo
     logger.info(f"Subnet alpha: {subnet_alpha}, subnet tao: {subnet_tao}, price: {price}")
     return amount_in_tao * price
 
+def print_extrinsic_receipt(receipt):
+    success_event = False
+    batch_interrupted_event = None
+    error_message = None
+    for event in receipt.triggered_events:
+        event_details = event.value['event']
+        module_id = event_details['module_id']
+        event_id = event_details['event_id']
+
+        if module_id == 'System' and event_id == 'ExtrinsicSuccess':
+            success_event = True
+        elif module_id == 'Utility' and event_id == 'BatchInterrupted':
+            batch_interrupted_event = event
+            error_message = int(event_details['attributes']['error']['Module']['error'], 16) >> 24
+
+    print(f"Extrinsic included in block: {receipt.block_hash}")
+    print(f"Extrinsic hash: {receipt.extrinsic_hash}")
+    print(f"Block number: {receipt.block_number}")
+    print(f"Events: {receipt.triggered_events}")
+
+    if batch_interrupted_event:
+        print("Extrinsic failed due to a batch interruption")
+        if error_message:
+            print(f"Error message: {error_message}")
+    elif success_event:
+        print("Extrinsic succeeded")
+    else:
+        print("Extrinsic failed")
+        if receipt.error_message:
+            print(f"Error message: {receipt.error_message}")
+
+def batch_transfer_balances(substrate: SubstrateInterface, kaypair, transfer_info_dict: dict):
+
+    all_calls = []
+    for dest_coldkey, amount_in_usd in transfer_info_dict:
+        
+        amount_in_tao, rate = coingecko_client.convert_to_tao(amount_in_usd)
+        amount_in_alpha = int(convert_to_alpha(substrate, amount_in_tao) * pow(10, 9))
+        
+        call = substrate.compose_call(
+            call_module="SubtensorModule",
+            call_function="transfer_stake",
+            call_params={
+                "destination_coldkey": dest_coldkey,
+                "hotkey": setting.hotkey,
+                "origin_netuid": setting.net_uid,
+                "destination_netuid": setting.net_uid,
+                "alpha_amount": amount_in_alpha,
+            },
+        )
+
+        all_calls.append(call)
+
+    batch_call = node.compose_call(
+        call_module="Utility",
+        call_function="force_batch",
+        call_params={'calls': all_calls}
+    )
+
+    extrinsic = substrate.create_signed_extrinsic(call=batch_call, keypair=keypair)
+    wait_for_inclusion = True
+    response = substrate.submit_extrinsic(extrinsic, wait_for_inclusion=wait_for_inclusion, wait_for_finalization=False)
+    if wait_for_inclusion:
+        print_extrinsic_receipt(response)   
+    return None, None
 
 def transfer_balance(substrate: SubstrateInterface, keypair, dest_coldkey: str, amount_in_usd: float):
     """
